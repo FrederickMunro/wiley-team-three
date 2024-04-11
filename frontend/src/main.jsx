1e6373b97d083a96951b9d587f07b0e50aca833b
FrederickMunro <fredmunro7@gmail.com>
2024-04-11 14:03:29 -0400

    Initialized basic router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx';
+
 import './main.css';
+
+
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>,
 );
