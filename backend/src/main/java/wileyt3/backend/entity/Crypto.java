535705d3443cc3e51a4967b9b0d9995cf761e596
Antoine.A <antoine.assal@mail.concordia.ca>
2024-04-12 20:17:59 -0400

    Add new entities/models

---
+++ backend/src/main/java/wileyt3/backend/entity/Crypto.java
@@ -0,0 +1,37 @@
+package wileyt3.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "crypto")
+public class Crypto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "ticker", nullable = false, length = 10)
+    private String ticker;
+
+    @Column(name = "base_currency", length = 10)
+    private String baseCurrency;
+
+    @Column(name = "quote_currency", length = 10)
+    private String quoteCurrency;
+
+    @Column(name = "last_price", precision = 16)
+    private Double lastPrice;
+}
---
+++ backend/src/main/java/wileyt3/backend/entity/Role.java
@@ -0,0 +1,29 @@
+package wileyt3.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+}
